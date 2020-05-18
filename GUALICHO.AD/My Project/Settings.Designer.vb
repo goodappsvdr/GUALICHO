4f9e0ef2242e0ca5d1d0c9c325c628ab682bc2f1
faculuq <56322143+faculuq@users.noreply.github.com>
2020-05-18 11:33:15 -0300

    Archivos proyecto

--- GUALICHO.AD/My Project/Settings.Designer.vb
+++ GUALICHO.AD/My Project/Settings.Designer.vb
@@ -62,11 +62,11 @@ Namespace My
      Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()>  _
     Friend Module MySettingsProperty
-        
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>  _
-        Friend ReadOnly Property Settings() As Global.HIERCONS.AD.My.MySettings
+
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>
+        Friend ReadOnly Property Settings() As Global.GUALICHO.AD.My.MySettings
             Get
-                Return Global.HIERCONS.AD.My.MySettings.Default
+                Return Global.GUALICHO.AD.My.MySettings.Default
             End Get
         End Property
     End Module
--- GUALICHO.BACKEND/My Project/Settings.Designer.vb
+++ GUALICHO.BACKEND/My Project/Settings.Designer.vb
@@ -62,10 +62,10 @@ Namespace My
      Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute()> _
     Friend Module MySettingsProperty
 
-        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")> _
-        Friend ReadOnly Property Settings() As Global.HIERCONS.BACKEND.My.MySettings
+        <Global.System.ComponentModel.Design.HelpKeywordAttribute("My.Settings")>
+        Friend ReadOnly Property Settings() As Global.GUALICHO.BACKEND.My.MySettings
             Get
-                Return Global.HIERCONS.BACKEND.My.MySettings.Default
+                Return Global.GUALICHO.BACKEND.My.MySettings.Default
             End Get
         End Property
     End Module
